14d49c69ec5696d2d04a41479615f7b0d3907f9d
sabrinnnnna <sabrina.kittunah@gmail.com>
2025-02-09 11:07:01 +0700

    display menu

---
+++ calculator.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+void displayMenu() {
+    cout << "Calculator Menu:" << endl;
+    cout << "1. Sum" << endl;
+    cout << "2. Divide" << endl;
+    cout << "3. Multiply" << endl;
+    cout << "4. Subtract" << endl;
+    cout << "5. Exit" << endl;
+    cout << "Choose an option: ";
+}
