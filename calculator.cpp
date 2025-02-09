303939dcf3cc4373e947198a77964f8b08e1f42f
sabrinnnnna <sabrina.kittunah@gmail.com>
2025-02-09 11:11:05 +0700

    calculate function

--- calculator.cpp
+++ calculator.cpp
@@ -11,3 +11,50 @@ void displayMenu() {
     cout << "5. Exit" << endl;
     cout << "Choose an option: ";
 }
+
+void performOperation(int choice) {
+    double num1, num2, result;
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    switch (choice) {
+        case 1:
+            result = num1 + num2;
+            cout << "Result: " << result << endl;
+            break;
+        case 2:
+            if (num2 != 0) {
+                result = num1 / num2;
+                cout << "Result: " << result << endl;
+            } else {
+                cout << "Error: Division by zero!" << endl;
+            }
+            break;
+        case 3:
+            result = num1 * num2;
+            cout << "Result: " << result << endl;
+            break;
+        case 4:
+            result = num1 - num2;
+            cout << "Result: " << result << endl;
+            break;
+        default:
+            cout << "Invalid choice!" << endl;
+            break;
+    }
+}
+
+int main() {
+    int choice;
+    do {
+        displayMenu();
+        cin >> choice;
+        if (choice >= 1 && choice <= 4) {
+            performOperation(choice);
+        }
+    } while (choice != 5);
+
+    return 0;
+}
